78ececce18ccd0192a01d4233062423b5a9d4dc2
VictorGiuliano <victorgiuliano98@gmail.com>
2022-12-13 17:33:36 +0100

    Somma dei numeri

--- js/snackone.js
+++ js/snackone.js
@@ -1,10 +1,18 @@
 /*
 Il software deve chiedere per 10 volte all’utente di inserire un numero. (qui potete usare un prompt). 
 Il programma stampa la somma di tutti i numeri inseriti.*/
-
-let number;
+const result = document.getElementById('result');
+const number=[];
+let addnumber;
+let sum = 0;
 //Richiesta di 10 volte per l'inserimento di numeri all'utente
 for(let i = 1; i<=10;i++){
-   number = parseInt(prompt('Inserisci un numero').trim());
-   console.log(number);
+   addnumber = parseInt(prompt('Inserisci un numero').trim());
+   number.push(addnumber);
 }
+console.log(number);
+//Somma dei valori assegnati dall'utente
+for(let i = 0; i<number.length; i++){
+ sum += number[i];
+}
+console.log(sum);
